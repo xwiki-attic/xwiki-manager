889da84421eaa79359a72050a915395ca6663761
tmortagne <thomas.mortagne@xwiki.com>
2007-11-29 14:58:04 +0000

    XEM-71: Add integration tests. Add selenium based tests from XE.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-products/xwiki-enterprise-manager/trunk@6143 f329d543-caf0-0310-9063-dda96c69346f

---
+++ distribution-test/selenium-tests/src/test/it/com/xpn/xwiki/it/selenium/framework/AlbatrossSkinExecutor.java
@@ -0,0 +1,249 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xpn.xwiki.it.selenium.framework;
+
+/**
+ * Implementation of skin-related actions for the Albatross skin.
+ * 
+ * @version $Id: $
+ */
+public class AlbatrossSkinExecutor implements SkinExecutor
+{
+    private static final String WYSIWYG_LOCATOR_FOR_KEY_EVENTS = "mceSpanFonts";
+
+    private static final String WIKI_LOCATOR_FOR_KEY_EVENTS = "content";
+
+    private AbstractXWikiTestCase test;
+
+    public AlbatrossSkinExecutor(AbstractXWikiTestCase test)
+    {
+        this.test = test;
+    }
+
+    private AbstractXWikiTestCase getTest()
+    {
+        return this.test;
+    }
+
+    public void clickDeletePage()
+    {
+        getTest().clickLinkWithLocator("//a[string() = 'Delete']");
+    }
+
+    public void clickEditPreview()
+    {
+        getTest().submit("formactionpreview");
+    }
+
+    public void clickEditSaveAndContinue()
+    {
+        getTest().submit("formactionsac");
+    }
+
+    public void clickEditCancelEdition()
+    {
+        getTest().submit("formactioncancel");
+    }
+
+    public void clickEditSaveAndView()
+    {
+        getTest().submit("formactionsave");
+    }
+
+    public boolean isAuthenticated()
+    {
+        return !(getTest().isElementPresent("headerlogin") && getTest().isElementPresent(
+            "headerregister"));
+    }
+
+    public void logout()
+    {
+        getTest().assertTrue("User wasn't authenticated.", isAuthenticated());
+        getTest().clickLinkWithLocator("headerlogout");
+        getTest().assertFalse("The user is still authenticated after a logout.",
+            isAuthenticated());
+    }
+
+    public void login(String username, String password, boolean rememberme)
+    {
+        getTest().open("/xwiki/bin/view/Main/");
+
+        if (isAuthenticated()) {
+            logout();
+        }
+
+        clickLogin();
+
+        getTest().setFieldValue("j_username", username);
+        getTest().setFieldValue("j_password", password);
+        if (rememberme) {
+            getTest().checkField("rememberme");
+        }
+        getTest().submit();
+
+        getTest().assertTrue("User has not been authenticated", isAuthenticated());
+    }
+
+    public void loginAsAdmin()
+    {
+        login("Admin", "admin", false);
+    }
+
+    public void clickLogin()
+    {
+        getTest().clickLinkWithLocator("headerlogin");
+        assertIsLoginPage();
+    }
+
+    private void assertIsLoginPage()
+    {
+        getTest().assertElementPresent("loginForm");
+        getTest().assertElementPresent("j_username");
+        getTest().assertElementPresent("j_password");
+        getTest().assertElementPresent("rememberme");
+    }
+
+    public void clickRegister()
+    {
+        getTest().clickLinkWithLocator("headerregister");
+        assertIsRegisterPage();
+    }
+
+    private void assertIsRegisterPage()
+    {
+        // tests that the register form exists
+        getTest().assertElementPresent("register");
+        getTest().assertElementPresent("register_first_name");
+        getTest().assertElementPresent("register_last_name");
+        getTest().assertElementPresent("xwikiname");
+        getTest().assertElementPresent("register_password");
+        getTest().assertElementPresent("register2_password");
+        getTest().assertElementPresent("register_email");
+    }
+
+    // For WYSIWYG editor
+
+    public void editInWysiwyg(String space, String page)
+    {
+        getTest().open("/xwiki/bin/edit/" + space + "/" + page + "?editor=wysiwyg");
+    }
+
+    public void clearWysiwygContent()
+    {
+        getTest().getSelenium().waitForCondition(
+            "selenium.browserbot.getCurrentWindow().tinyMCE.setContent(\"\"); true", "18000");
+    }
+
+    public void typeInWysiwyg(String text)
+    {
+        getTest().getSelenium().typeKeys(WYSIWYG_LOCATOR_FOR_KEY_EVENTS, text);
+    }
+
+    public void typeInWiki(String text)
+    {
+        getTest().getSelenium().type(WIKI_LOCATOR_FOR_KEY_EVENTS, text);
+    }
+
+    public void typeEnterInWysiwyg()
+    {
+        getTest().getSelenium().keyPress(WYSIWYG_LOCATOR_FOR_KEY_EVENTS, "\\13");
+    }
+
+    public void typeShiftEnterInWysiwyg()
+    {
+        getTest().getSelenium().shiftKeyDown();
+        getTest().getSelenium().keyPress(WYSIWYG_LOCATOR_FOR_KEY_EVENTS, "\\13");
+    }
+
+    public void clickWysiwygUnorderedListButton()
+    {
+        getTest().clickLinkWithLocator("//img[@title='Unordered list']", false);
+    }
+
+    public void clickWysiwygOrderedListButton()
+    {
+        getTest().clickLinkWithLocator("//img[@title='Ordered list']", false);
+    }
+
+    public void clickWysiwygIndentButton()
+    {
+        getTest().clickLinkWithLocator("//img[@title='Indent']", false);
+    }
+
+    public void clickWysiwygOutdentButton()
+    {
+        getTest().clickLinkWithLocator("//img[@title='Outdent']", false);
+    }
+
+    public void clickWikiBoldButton()
+    {
+        getTest().clickLinkWithXPath("//img[@title='Bold']", false);
+    }
+
+    public void clickWikiItalicsButton()
+    {
+        getTest().clickLinkWithXPath("//img[@title='Italics']", false);
+    }
+
+    public void clickWikiUnderlineButton()
+    {
+        getTest().clickLinkWithXPath("//img[@title='Underline']", false);
+    }
+
+    public void clickWikiLinkButton()
+    {
+        getTest().clickLinkWithXPath("//img[@title='Internal Link']", false);
+    }
+
+    public void clickWikiHRButton()
+    {
+        getTest().clickLinkWithXPath("//img[@title='Horizontal ruler']", false);
+    }
+
+    public void clickWikiImageButton()
+    {
+        getTest().clickLinkWithXPath("//img[@title='Attached Image']", false);
+    }
+
+    public void clickWikiSignatureButton()
+    {
+        getTest().clickLinkWithXPath("//img[@title='sig']", false);
+    }
+
+    public void assertWikiTextGeneratedByWysiwyg(String text)
+    {
+        getTest().clickLinkWithText("Wiki");
+        getTest().assertEquals(text, getTest().getSelenium().getValue("content"));
+    }
+
+    public void assertHTMLGeneratedByWysiwyg(String xpath) throws Exception
+    {
+        getTest().getSelenium().selectFrame("mce_editor_0");
+        getTest().assertTrue(
+            getTest().getSelenium().isElementPresent("xpath=/html/body/" + xpath));
+        getTest().getSelenium().selectFrame("relative=top");
+    }
+
+    public void assertGeneratedHTML(String xpath) throws Exception
+    {
+        getTest().assertTrue(
+            getTest().getSelenium().isElementPresent("xpath=//div[@id='xwikicontent']/" + xpath));
+    }
+}
---
+++ distribution-test/selenium-tests/src/test/it/com/xpn/xwiki/it/selenium/framework/SkinExecutor.java
@@ -0,0 +1,212 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xpn.xwiki.it.selenium.framework;
+
+/**
+ * Skin-related actions to be implemented by the different Skin Executors. A Skin Executor is simply
+ * a class extending this interface and implementing the listed UI actions.
+ * 
+ * @version $Id: $
+ */
+public interface SkinExecutor
+{
+    /**
+     * Click on the Delete button leading to the page for deleting the current page.
+     */
+    void clickDeletePage();
+
+    /**
+     * Click on the Preview button in Edit mode to preview the changed made to a page.
+     */
+    void clickEditPreview();
+
+    /**
+     * Click on the Save & Continue button in Edit mode to save the page and continue editing it.
+     */
+    void clickEditSaveAndContinue();
+
+    /**
+     * Click on the Cancel button in Edit mode to cancel the modifications to a page.
+     */
+    void clickEditCancelEdition();
+
+    /**
+     * Click on the Save & View button in Edit mode to save the page and view the result.
+     */
+    void clickEditSaveAndView();
+
+    /**
+     * @return true if there's a user logged in or false otherwise
+     */
+    boolean isAuthenticated();
+
+    /**
+     * Logs out the current user.
+     */
+    void logout();
+
+    /**
+     * Login the passed user.
+     * 
+     * @param username name of the user to log in
+     * @param password password of the user to log in
+     * @param rememberme if true the user will not have to log in again when he comes back
+     */
+    void login(String username, String password, boolean rememberme);
+
+    /**
+     * Logs in the Admin user.
+     */
+    void loginAsAdmin();
+
+    /**
+     * Click on the Login button leading to the login page.
+     */
+    void clickLogin();
+
+    /**
+     * Click on the Register button
+     */
+    void clickRegister();
+
+    // For WYSIWYG editor
+
+    /**
+     * Edit the passed space/page using the WYSIWYG editor.
+     * 
+     * @param space the space to which the page to edit belongs to
+     * @param page the page to edit
+     */
+    void editInWysiwyg(String space, String page);
+
+    /**
+     * Clears the content of the current page being edited in WYSIWYG mode
+     */
+    void clearWysiwygContent();
+
+    /**
+     * Type the passed text in the WYSIWYG editor.
+     * 
+     * @param text the text to be added to the WYSIWYG editor content
+     */
+    void typeInWysiwyg(String text);
+
+    /**
+     * Type the passed text in thw Wiki editor.
+     * 
+     * @param text the text to be added to the Wiki editor content
+     */
+    void typeInWiki(String text);
+
+    /**
+     * Press Enter in the WYSIWYG editor.
+     */
+    void typeEnterInWysiwyg();
+
+    /**
+     * Press Shift + Enter in the WYSIWYG editor.
+     */
+    void typeShiftEnterInWysiwyg();
+
+    /**
+     * Clicks the WYSIWYG editor button to removed an ordered list.
+     */
+    void clickWysiwygUnorderedListButton();
+
+    /**
+     * Clicks the WYSIWYG editor button to create an ordered list.
+     */
+    void clickWysiwygOrderedListButton();
+
+    /**
+     * Clicks the WYSIWYG editor button to indent the text at the cursor position.
+     */
+    void clickWysiwygIndentButton();
+
+    /**
+     * Clicks the WYSIWYG editor button to un-indent the text at the cursor position.
+     */
+    void clickWysiwygOutdentButton();
+
+    /**
+     * Clicks the Wiki editor button to make the selected text bold, or to enter a bold marker if no
+     * text is selected.
+     */
+    void clickWikiBoldButton();
+
+    /**
+     * Clicks the Wiki editor button to make the selected text italics, or to enter an italics
+     * marker if no text is selected.
+     */
+    void clickWikiItalicsButton();
+
+    /**
+     * Clicks the Wiki editor button to make the selected text underlined, or to enter an underline
+     * marker if no text is selected.
+     */
+    void clickWikiUnderlineButton();
+
+    /**
+     * Clicks the Wiki editor button to turn the selected text into a link, or to enter a new link
+     * if no text is selected.
+     */
+    void clickWikiLinkButton();
+
+    /**
+     * Clicks the Wiki editor button to insert a new horizontal ruler.
+     */
+    void clickWikiHRButton();
+
+    /**
+     * Clicks the Wiki editor button to insert an image macro.
+     */
+    void clickWikiImageButton();
+
+    /**
+     * Clicks the Wiki editor button to insert a signature.
+     */
+    void clickWikiSignatureButton();
+
+    /**
+     * Verify that the WYSIWYG editor has generated the passed text when the page is viewed in the
+     * Wiki editor.
+     * 
+     * @param text the text to verify
+     */
+    void assertWikiTextGeneratedByWysiwyg(String text);
+
+    /**
+     * Verify that the WYSIWYG editor has generated HTML content matching the passed XPath
+     * expression, without having to save the edited document.
+     * 
+     * @param xpath the XPath expression to check
+     * @throws Exception in case of a XPath parsing exception
+     */
+    void assertHTMLGeneratedByWysiwyg(String xpath) throws Exception;
+
+    /**
+     * Verify that the XWiki editor (be it Wiki or WYSIWYG) has generated HTML matching the passed
+     * XPath expression when the document has been saved.
+     * 
+     * @param xpath the XPath expression to check
+     * @throws Exception in case of a XPath parsing exception
+     */
+    void assertGeneratedHTML(String xpath) throws Exception;
+}
