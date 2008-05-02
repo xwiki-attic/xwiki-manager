8cc490c2cd2afaa1d0eeaee4f43ebe512cc995c2
tmortagne <thomas.mortagne@xwiki.com>
2008-05-02 17:16:33 +0000

    XAWM-60: Validate provided wiki name using ajax * Fix selenium tests timing
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-products/xwiki-enterprise-manager/trunk@9620 f329d543-caf0-0310-9063-dda96c69346f

--- distribution-test/selenium-tests/src/test/it/com/xpn/xwiki/it/selenium/xem/WikiManagementCreate.java
+++ distribution-test/selenium-tests/src/test/it/com/xpn/xwiki/it/selenium/xem/WikiManagementCreate.java
@@ -80,21 +80,22 @@ public class WikiManagementCreate extends AbstractXWikiTestCase
      */
     private void setWikiNameFieldValue()
     {
-        getSelenium().setSpeed("1000");
-        
         // Validate that an existing wiki name is invalid as wiki name
         setFieldValue("wikiname", "xwiki");
         getSelenium().keyUp("wikiname", "\\40");
+        getSelenium().waitForCondition("selenium.page().bodyText().indexOf('This identifier is already used') != -1;", "10000");
         assertTextPresent("This identifier is already used");
 
         // Validate that "" is invalid as wiki name
         setFieldValue("wikiname", "");
         getSelenium().keyUp("wikiname", "\\40");
+        getSelenium().waitForCondition("selenium.page().bodyText().indexOf('Identifier can't be empty') != -1;", "10000");
         assertTextPresent("Identifier can't be empty");
 
         // Validate that a not existing wiki name is valid as wiki name
         setFieldValue("wikiname", "newemptywiki");
         getSelenium().keyUp("wikiname", "\\40");
+        getSelenium().waitForCondition("selenium.page().bodyText().indexOf('You can use this name as new wiki identifier') != -1;", "10000");
         assertTextPresent("You can use this name as new wiki identifier");
     }
 }
